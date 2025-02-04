08b12dd07eb9e702b808189c3ffeac921f21e62d
aasthananda <77813827+aasthananda@users.noreply.github.com>
2025-02-04 15:00:16 -0800

    Create code.py
    code

---
+++ code.py
@@ -0,0 +1,8 @@
+all =  ["a", 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+setnence = " "
+smth = str(input("Enter whatever values you want to implement"))
+for each in smth:
+    if each in all:
+        new = all[(all.index(each)+3)%26]
+        setnence += new
+print(setnence)
